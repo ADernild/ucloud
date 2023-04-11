4f23df44a3a4cd4082ac83ea507a6ce073cc04da
Alexander Dernild <aid@sdu.dk>
2023-04-11 12:28:34 +0200

    Fixing bug where pgbouncer wont be installed if wget already is installed

--- pgbouncer/_dep.sh
+++ pgbouncer/_dep.sh
@@ -17,12 +17,10 @@ if ! _ucld_::is_package_installed pgbouncer; then
     ## Adding PGDG apt repository
     wget -O - http://apt.postgresql.org/pub/repos/apt/ACCC4CF8.asc | apt-key add -
     su -c "echo 'deb http://apt.postgresql.org/pub/repos/apt/ $(lsb_release -sc)-pgdg main' >> /etc/apt/sources.list.d/pgdg.list"
-
-
-    apt-get update \
-      && apt-get install -y pgdg-keyring
-    apt-get install -y pgbouncer
   fi
+  apt-get update \
+      && apt-get install -y pgdg-keyring
+  apt-get install -y pgbouncer
 fi
 
 
