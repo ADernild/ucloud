9ba9b355e669c4e1b165c55ff65b709f33b403fd
Alexander Dernild <aid@sdu.dk>
2023-04-17 22:55:37 +0200

    added tls keys and crt to pgbouncer config generator

--- pgbouncer/_construct_ini.sh
+++ pgbouncer/_construct_ini.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 
 # shellcheck source=/dev/null
+{
+  . "incl/all.sh"
+  . "postgresql/_pg_globals.sh"
+  . "postgresql/_utils.sh"
+  . "postgresql/_create.sh"
+  . "postgresql/_ssl.sh"
+  # more files
+}
+
+# shellcheck source=/dev/null
 source ../env/.env
 
 DIR=etc/pgbouncer/
@@ -15,6 +25,10 @@ listen_port = 6432
 listen_addr = 0.0.0.0
 auth_type = trust
 auth_file = userlist.txt
+client_tls_sslmode = require
+client_tls_key_file = ${UCLD_PATH[database]}/server.key
+client_tls_cert_file = ${UCLD_PATH[database]}/server.crt
+client_tls_ca_file = ${UCLD_PATH[database]}/root.key
 server_tls_sslmode = require
 logfile = pgbouncer.log
 pidfile = pgbouncer.pid
